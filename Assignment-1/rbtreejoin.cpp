3282b43486592f884e31c83717b03193f50e6529
26prajval98 <26prajval98@gmail.com>
2019-03-05 13:08:29 +0530

    Done

--- Assignment-1/rbtreejoin.cpp
+++ Assignment-1/rbtreejoin.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include "rbtree.h"
 
-RBtree joinRBtrees2to1(RBtree tree1, RBtree tree2){
+RBtree joinRBtrees2to1(RBtree tree1, RBtree tree2, int val){
     node * t1_s = tree1.root;
     node * t2_s = tree2.root;
-    node * max = t1_s;
-    
-    while(max->right != NULL)
-        max = max->right;
-    
-    int val = max->key;
 
-    tree1.del(val);
-    
     while(black_height(t1_s) > black_height(t2_s)){
         t1_s = t1_s->right;
     }
@@ -33,22 +25,14 @@ RBtree joinRBtrees2to1(RBtree tree1, RBtree tree2){
 
     if(pp == NULL)
         p->color = 'b';
-        
+
     RBtree tree3(tree1.root);
     return tree3;
 }
 
-RBtree joinRBtrees1to2(RBtree tree1, RBtree tree2){
+RBtree joinRBtrees1to2(RBtree tree1, RBtree tree2, int val){
     node * t1_s = tree1.root;
     node * t2_s = tree2.root;
-    node * min = t2_s;
-    
-    while(min->left != NULL)
-        min = min->left;
-    
-    int val = min->key;
-
-    tree2.del(val);
     
     while(black_height(t1_s) < black_height(t2_s)){
         t2_s = t2_s->left;
@@ -79,10 +63,15 @@ RBtree joinRBtrees1to2(RBtree tree1, RBtree tree2){
 RBtree join_t1st2(RBtree tree1, RBtree tree2){
     node * t1_s = tree1.root;
     node * t2_s = tree2.root;
-    if(black_height(t1_s) > black_height(t2_s))
-        return joinRBtrees2to1(tree1, tree2);
+    int val = tree1.max();
+
+    tree1.del(val);
+
+    if(black_height(t1_s) > black_height(t2_s)){
+        return joinRBtrees2to1(tree1, tree2, val);
+    }
     else
-        return joinRBtrees1to2(tree1, tree2);
+        return joinRBtrees1to2(tree1, tree2, val);
 }
 
 RBtree join(RBtree tree1, RBtree tree2){
