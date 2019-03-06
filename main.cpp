4d83be4b3808618c5924f3df8a4df20fc6a9537f
Tejas1908 <36530921+Tejas1908@users.noreply.github.com>
2019-03-06 19:42:04 +0530

    AVL done

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include "AVL.h"
+using namespace std;
+
+int main()
+{
+	AVL<int> tree1, tree2;
+	int opt = 0;
+	do
+	{
+		cout<<"1. Insert \n2. Delete \n3. Display \n4. Merge \n5. Split \n0. Exit\n";
+		cin>>opt;
+		int val;
+		switch(opt)
+		{
+		case 1:
+		    cout<<"Enter value : ";
+			cin>>val;
+			tree1.ins(val);
+			break;
+		case 2:
+		    cout<<"Enter value : ";
+			cin>>val;
+			tree1.del(val);
+			break;
+		case 3:
+			tree1.disp();
+			break;
+		case 4:
+			{
+				int N;
+				cout<<"Enter number of nodes in smaller tree\n";
+				cin>>N;
+				cout<<"Enter node values\n";
+				while(N)
+				{
+					int x;
+					cin>>x;
+					tree2.ins(x);
+					--N;
+				}
+				tree1.add(tree2);
+			}
+			break;
+		case 5:
+			{
+				int ind;
+				cout<<"Enter key to split using\n";
+				cin>>ind;
+				tree1.split(ind).disp();
+				tree1.disp();
+			}
+			break;
+		}
+	}while(opt != 0);
+	return 0;
+}
